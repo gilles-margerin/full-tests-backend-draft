031a4928787ee35115b8605ecca312903ecd3a02
Gilles Margerin <gillesmargerin@gmail.com>
2021-08-06 06:40:16 +0200

    added examples of app methods in app.js

--- lvl1/app.js
+++ lvl1/app.js
@@ -1,6 +1,19 @@
 import App from "./domain/App.js";
 
-const app = new App()
+const app = new App();
+
+// exposing app
+const aliceUser = app.createUser("Alice Foobaz");
+const aliceDefaultFleet = app.createFleet(aliceUser.id);
+const aliceVehicle = app.registerVehicle(aliceDefaultFleet, "123-XY-456");
+const aliceParkLocation = app.parkVehicle(aliceDefaultFleet, "123-XY-456", "45.89", "12.78", "1250")
+
+console.log({
+  userCreation: aliceUser,
+  fleetCreation: aliceDefaultFleet,
+  registerVehicule: aliceVehicle,
+  parkVehicle: aliceParkLocation
+})
 
 export default app;
 
