907f0eca4fbe0bc20c7c6f77813db479273beb2f
Gilles Margerin <gillesmargerin@gmail.com>
2021-08-08 21:43:47 +0200

    fixed mutliple fleets refs

--- lvl2/handlers/createHandler.js
+++ lvl2/handlers/createHandler.js
@@ -3,7 +3,40 @@ import Fleet from "../domain/Fleet.js";
 
 export default async function createHandler(userId, client, connection) {
   try {
-    const user = new User({
+    const db = await connection(client);
+    const user = await db.collection("users").findOne({ _id: userId });
+
+    if (user) {
+      const fleet = new Fleet({
+        _id: Math.floor(Math.random() * (999999999 - 1 + 1) + 1),
+        vehicles: new Map(),
+        userId: userId,
+      });
+      
+      await db.collection("users").updateOne({ _id: userId }, {
+        $push : {
+          fleetsRefs: fleet._id
+        }
+      })
+      const result = await db.collection("fleets").insertOne(fleet);
+      console.log(result)
+    } else {
+      const newUser = new User({
+        _id: userId,
+        fleetsRefs: [Math.floor(Math.random() * (999999999 - 1 + 1) + 1)],
+      });
+
+      const fleet = new Fleet({
+        _id: newUser.fleetsRefs[0],
+        vehicles: new Map(),
+        userId: userId,
+      });
+  
+      const result = await db.collection("users").insertOne(newUser);
+      const result2 = await db.collection("fleets").insertOne(fleet);
+      console.log(result, result2)
+    }
+    /* const user = new User({
       _id: userId,
       fleetsRefs: [Math.floor(Math.random() * (999999999 - 1 + 1) + 1)],
     });
@@ -21,7 +54,7 @@ export default async function createHandler(userId, client, connection) {
     console.log({
       userCreated: newUser.insertedId,
       defaultFleetId: user.fleetsRefs[0],
-    });
+    }); */
   } catch (err) {
     console.error(err);
     return err;
--- lvl2/handlers/registerHandler.js
+++ lvl2/handlers/registerHandler.js
@@ -1,29 +1,36 @@
 import Vehicle from "../domain/Vehicle.js";
 
-export default async function registerHandler(fleetId, plate, client, connection) {
+export default async function registerHandler(
+  fleetId,
+  plate,
+  client,
+  connection
+) {
   try {
     const vehicle = new Vehicle({
       plate: plate,
       parked: false,
-      lng: 0.000,
-      lat: 0.000,
-      alt: 0.000
+      lng: 0.0,
+      lat: 0.0,
+      alt: 0.0,
     });
 
     const db = await connection(client);
-    const result = await db.collection("fleets").findOneAndUpdate(
+    await db.collection("fleets").findOneAndUpdate(
       { _id: Number(fleetId) },
       {
         $set: {
-          [`vehicles.${plate}`]: vehicle
+          [`vehicles.${plate}`]: vehicle,
         },
       },
       {
-        upsert: true
+        upsert: true,
       }
     );
 
-    console.log(result);
+    console.log({
+      registeredVehicle: plate,
+    });
   } catch (err) {
     console.error(err);
   } finally {
